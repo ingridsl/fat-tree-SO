2326641900bd1e5457c9fbc680516a77b33e6042
EdgarFabiano <eddy.led@gmail.com>
2017-05-16 20:32:55 -0300

    Timer para a execução postergada

--- executa_postergado.cpp
+++ executa_postergado.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <string>
 
 #define N 100
@@ -13,12 +14,21 @@ int main (int argc, char *argv[]){
 		exit(1);
 	}
 
-	int seq = atoi(argv[1]); 
+	int seg = atoi(argv[1]); 
 	char arq_executavel[N];
 	strcpy(arq_executavel, argv[2]);
-	printf("%d, %s\n", seq, arq_executavel);
+	printf("%d, %s\n", seg, arq_executavel);
 
-	/* se não houver parâmetro invalido, é atribuido um número de job único à tupla <arq_exec><seq> q é colocada numa estrutura de dados compartilhada*/
+	/*Se não houver parâmetro invalido, é atribuido um número de job único à tupla <arq_exec><seq> q é colocada numa estrutura de dados compartilhada*/
+
+	time_t start = time(NULL);
+
+	while(1) 
+		if(difftime(start, time(NULL))*(-1) == seg)
+			break;
+
+	printf("\nInicio da execucao apos %d segundos\n", seg);
+	/*A partir daqui o processo escalonador de execução solicita que todos os processos gerentes de execução executem o programa <arq_executavel>, marcando-os como ocupados*/
 
 return 0;
 }
