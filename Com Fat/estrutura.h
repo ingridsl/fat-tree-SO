ac8faffff5a3af490c2d18fa93668335197f8ef4
renneruan <140030930@aluno.unb.br>
2017-05-22 23:10:56 -0300

    Pasta com a fat tree separada

---
+++ Com Fat/estrutura.h
@@ -0,0 +1,55 @@
+#ifndef ESTRUTURA_H
+#define ESTRUTURA_H
+
+#define N 200
+#define RANGE_REQUISICOES -50
+#define ADICIONA_JOB 49
+#define SHUTDOWN 50
+#define CONFIRMACAO_EXECUCAO 51
+#define N_NOS 15
+
+#define ID_1 1
+#define ID_2 2
+#define ID_3 3
+#define ID_4 4
+#define ID_5 5
+#define ID_6 6
+#define ID_7 7
+#define ID_8 8
+#define ID_9 9
+#define ID_10 10
+#define ID_11 11
+#define ID_12 12
+#define ID_13 13
+#define ID_14 14
+#define ID_15 15
+
+int fator_round_robin[4] = {1,4,5,6};
+int index_round_robin;
+
+/**/
+struct mensagem {
+	long tipo; // tipo da mensagem, para o escalonador ser usado = 49
+	int job; // ?
+	char arq[N]; // nome do arquivo
+	int delay; // segundos da postergacao
+};
+
+/**/
+struct job {
+	long tipo;
+	int job;
+};
+
+/**/
+struct exec{
+	long tipo;
+	int job;
+	char arq[N];
+	int tempo; //delay
+	int tempo_solicitacao;
+	int tempo_inicio;
+	int tempo_termino;
+};
+
+#endif
---
+++ Com Fat/gerente.h
@@ -0,0 +1,46 @@
+#ifndef GERENTE_H
+#define GERENTE_H
+
+#include "estrutura.h"
+#include "executa_postergado.h"
+
+#define OCUPADO 1
+#define LIVRE 0
+
+/** */
+int identificador;
+/** */
+int pid_filho_1, pid_filho_2;
+/** */
+int msgqid_up, msgqid_down;
+/** */
+int estado;
+
+/**/
+struct exec msg, msgfilho;
+
+/** 
+* @param 
+* @return 
+*/
+std::string obterNomeArq(char *arq);
+
+/** 
+* @param 
+* @return 
+*/
+int obterHorarioAtual();
+
+/** 
+* @param 
+* @return 
+*/
+void shutdown(int sig);
+
+/** 
+* @param 
+* @return 
+*/
+void trabalha();
+
+#endif
