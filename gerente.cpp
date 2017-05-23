eef6ac79a9ccba139ad633984e2632aa63a4b9f9
renneruan <140030930@aluno.unb.br>
2017-05-22 21:39:20 -0300

    Comentários gerente

--- gerente.cpp
+++ gerente.cpp
@@ -30,13 +30,13 @@ int obterHorarioAtual(){
 void shutdown(int sig){
 	int status1, status2;
 
-	if(identificador == 1 && estado == OCUPADO){
+	if(identificador == ID_1 && estado == OCUPADO) //A verificacao para ID_1 serve apenas para verificar apenas 1 job
 		printf("Job que está executando sob a solicitação de shutdown: %d, arquivo %s\n", msg.job, msg.arq);
-	}
-	if(identificador < 8){ //Exclui nivel 4 da arvore
-		kill(pid_filho_1, SIGUSR1);
+	
+	if(identificador < ID_8){ //Envia sinais de shutdown para os filhos (somente os ńos que possuem filhos enviam)
+		kill(pid_filho_1, SIGUSR1); 
 		kill(pid_filho_2, SIGUSR1);
-		wait(&status1);
+		wait(&status1); //Espera o termino da execucao dos filhos que receberam o sinal de termino 
 		wait(&status2);
 	}
 
@@ -47,70 +47,78 @@ void trabalha(){
 	int pid_aux;
 	int msgsize = sizeof(struct exec) - sizeof(long);
 	int wait_status;
-	int identificador_pai = identificador/2;
+	int identificador_pai = identificador/2; //Calculo básico para acha o ID do no pai (considerando arredondamento)
 
-	if(identificador < 8){
+	//O gerente irá trabalhar de maneira diferente para os nós que possuem ou não possuem filhos
+	if(identificador < ID_8){
 		while(true){
-			
+			//Verifica mensagens que são recebidas do escalonador (de execucao)
 			if(msgrcv(msgqid_down, &msg, msgsize, identificador, IPC_NOWAIT) != -1){
-			
+				
 				msgfilho = msg;
-				msgfilho.tipo = identificador*2;
+				//Envia as mensagens para os filhos do nó na árvore
+				//Envia a mesma mensagem, porém com destinos (tipo) diferente
+				msgfilho.tipo = identificador*2; //Calculo basico para achar identificador do filho
 				msgsnd(msgqid_down, &msgfilho, msgsize ,0);
-				msgfilho.tipo = identificador*2+1;
+				msgfilho.tipo = identificador*2+1; //Calculo basico para achar identificador do filho
 				msgsnd(msgqid_down, &msgfilho, msgsize ,0);
 
+				//Controle de acesso de execucao do Gerente
 				if(estado == LIVRE){
 					estado = OCUPADO;
 					
+					//Cria processo para execucao do arquivo solicitado
 					pid_aux = fork();
-					if(pid_aux < 0 ){
-						printf("Erro na criação de processo a partir do Fork()\n"); exit(1);
-					}
+					if(pid_aux < 0 ){ printf("Erro na criação de processo a partir do Fork()\n"); exit(1); }
 					if(pid_aux == 0){
 						const char* nome_arq = obterNomeArq(msg.arq).c_str();
-						execl(msg.arq, nome_arq, (char*) NULL);
-					}else{
+						execl(msg.arq, nome_arq, (char*) NULL); //Execucao de processo filho do gerente
+					}else{ //Gerente entra em modo espera até o termino da execucao do arquivo
 						wait(&wait_status);
 
-						if(identificador != 1){
+						//Utiliza dessa struct exec para mandar informacoes acerca do termino da execucao para o escalonador
+						//Necessario para verificar a possibilidade de envio de um novo Job para os gerentes
+						if(identificador != ID_1){ //Se não for no 1, direciona confirmacao para o pai
 							msg.tipo = identificador_pai;
-						}else{
-							msg.tipo = 51;
+						}else{ //Se for o no 1 manda direto para o escalonador
+							msg.tipo = CONFIRMACAO_EXECUCAO;
 						}
 						msg.tempo_termino = obterHorarioAtual();
+						//Utiliza a fila de envio para o escalonador
 						msgsnd(msgqid_up, &msg, msgsize ,0);
 						estado = LIVRE; 
 					}		
 				}
 			}
+			//Caso um no intermediario receba uma confirmacao (Fila de mensagens UP) reenvia para o pai ate chegar
+			//ao escalonador
 			if(msgrcv(msgqid_up, &msg, msgsize, identificador, IPC_NOWAIT) != -1){
-				if(identificador != 1){
+				if(identificador != ID_1){ //Se não for no 1, direciona confirmacao para o pai
 						msg.tipo = identificador_pai;
-					}else{
-						msg.tipo = 51;
+					}else{ //Se for o no 1 manda direto para o escalonador
+						msg.tipo = CONFIRMACAO_EXECUCAO;
 				}
 				msgsnd(msgqid_up, &msg, msgsize ,0);
 			}
 		}
+	//Execucao para nos sem filhos diferenciada (não haverá propagacao, nem verificar se recebe confirmacoes)
 	}else{
 		while(true){
+			//Verifica o recebimento de solicitacao de execucao
 			msgrcv(msgqid_down, &msg, msgsize, identificador, 0);
 			if(estado == LIVRE){
 				estado = OCUPADO;
 				pid_aux = fork();
-				if(pid_aux < 0 ){
-					printf("Erro na criação de processo a partir do Fork()\n"); exit(1);
-				}
+				if(pid_aux < 0 ){ printf("Erro na criação de processo a partir do Fork()\n"); exit(1); }
 				if(pid_aux == 0){
 					const char* nome_arq = obterNomeArq(msg.arq).c_str();
-					execl(msg.arq, nome_arq, (char*) NULL);
+					execl(msg.arq, nome_arq, (char*) NULL); //Executa arquivo
 				}else{
-					wait(&wait_status);
+					wait(&wait_status); //Gerente espera o fim do programa executado
 					estado = LIVRE; 
 					msg.tempo_termino = obterHorarioAtual();
 					msg.tipo = identificador_pai;
-					msgsnd(msgqid_up, &msg, msgsize ,0);
+					msgsnd(msgqid_up, &msg, msgsize ,0); //Manda confirmacao de termino da execucao para o pai
 				}		
 			}
 		}
@@ -120,27 +128,28 @@ void trabalha(){
 int main (int argc, char *argv[]){
 	signal(SIGUSR1, shutdown);
 
+	//Conversão de parametros recebidos
 	identificador = atoi(argv[1]);
 	if(argc > 2){
 		pid_filho_1 = atoi(argv[2]);
 		pid_filho_2 = atoi(argv[3]);
 	}
 
-	//FILA DE VOLTA
+	//Fila de mensagens direcionadas ao escalonador ("Volta/Subida")
 	key_t msgkey_up = 0x14002713;
 	if((msgqid_up = msgget(msgkey_up, 0x1B6)) < 0){ //permissão: 110 110 110 (RWX)
 		printf("Erro na obtencao da fila a partir do msgget");
 		exit(1);
    	}
 
-	//FILA DE IDA
+	//Fila de mensagens que saem do escalonador ("Ida/Descida")
 	key_t msgkey_down = 0x14002712;
 	if((msgqid_down = msgget(msgkey_down, 0x1B6)) < 0){//permissão: 110 110 110 (RWX)
 		printf("Erro na obtencao da fila a partir do msgget");
 		exit(1);
    	 }
 
-	trabalha();
+	trabalha(); //Rotina de funcionamento do gerente
 
 	return 0;
 }
